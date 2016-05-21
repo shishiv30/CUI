522d19e9eb479e4623eb0bac0e66339f91ea1e72
Conjee <czou@movoto.com>
2016-05-21 11:42:45 +0800

    update grid table

--- src/js/plugins/_extend.js
+++ src/js/plugins/_extend.js
@@ -1,20 +1,61 @@
 if (!String.prototype.format) {
-  String.prototype.format = function () {
-      var args = arguments;
-      return this.replace(/{(\d+)}/g, function (match, number) {
-          return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-          ;
-      });
-  };
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
 }
-(function($){
-  $.isMobile =function(){
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      return true;
-    }else{
-      return false;
-    }
-  }
+var tmpdiv = null;
+(function ($) {
+    $.extend({
+        getTextWidth: function (text, fontsize) {
+            fontSize = fontsize || $('body').css('fontSize').replace(/[a-z]/g,'') * 1;
+            if (!tmpdiv) {
+                tmpdiv = $("<div></div>").css({
+                    position: 'absolute',
+                    visibility: 'hidden',
+                    height: 'auto',
+                    width: 'auto',
+                    whiteSpace: 'nowrap'
+                });
+                $('body').append(tmpdiv);
+            }
+            tmpdiv.css('fontSize',fontsize);
+            tmpdiv.text(text);
+            return tmpdiv.width();
+        },
+        insertCSS: function (selector, rules, contxt) {
+            var context = contxt || document,
+                stylesheet;
+
+            if (typeof context.styleSheets == 'object') {
+                if (context.styleSheets.length) {
+                    stylesheet = context.styleSheets[context.styleSheets.length - 1];
+                }
+                if (context.styleSheets.length) {
+                    if (context.createStyleSheet) {
+                        stylesheet = context.createStyleSheet();
+                    } else {
+                        context.getElementsByTagName('head')[0].appendChild(context.createElement('style'));
+                        stylesheet = context.styleSheets[context.styleSheets.length - 1];
+                    }
+                }
+                if (stylesheet.addRule) {
+                    for (var i = 0; i < selector.length; ++i) {
+                        stylesheet.addRule(selector[i], rules);
+                    }
+                } else {
+                    stylesheet.insertRule(selector.join(',') + '{' + rules + '}', stylesheet.cssRules.length);
+                }
+            }
+        },
+        isMobile: function () {
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
 })(jQuery);
--- src/js/plugins/gridtable.js
+++ src/js/plugins/gridtable.js
@@ -1,43 +1,61 @@
-(function($) {
-    $.fn.gridtable = function(option) {
+(function ($) {
+    $.fn.gridtable = function (option) {
         var defaultOption = {
-            columns: [{
-                text: '',
-                colspan: 1
-            }]
+            limitwidth: 150,
+            key: 'thead th',
+            limitheight: 35
         };
         var opt = $.extend({}, defaultOption, option);
         var $this = $(this);
-        var inital = function() {
+        var $key = $this.find(opt.key);
+        var $list = $this.find('body tr');
+        var inital = function () {
             var classname = 'table-' + +new Date();
             var colIndex = 0;
-            for (var i = 0; i < opt.columns.length; i++) {
-                var column = opt.columns[i];
+            var fontsize = $key.css('fontSize').replace(/[a-z]/g, '');
+            var keymaxwidth = 0;
+            var columns = $key.map(function (index, item) {
+                return {
+                    text: $(item).text() || '',
+                    colspan: $(item).attr('colspan') * 1 || 1
+                };
+            });
+            for (var i = 0; i < columns.length; i++) {
+                var column = columns[i];
                 colIndex = colIndex + 1;
-                $.insertCSS(['.'+classname + ' td:nth-of-type(' + colIndex + '):before'], 'content:"' + column.text + '";');
+                $.insertCSS(['.' + classname + ' td:nth-of-type(' + colIndex + '):before'], 'content:"' + column.text + ':";');
                 if (column.colspan > 1) {
-                    colIndex = colIndex + column.colspan-1;
+                    colIndex = colIndex + column.colspan - 1;
+                }
+                var keywidth = $.getTextWidth(column.text, fontsize);
+                if (keywidth > keymaxwidth) {
+                    keymaxwidth = keywidth;
                 }
             }
+            keymaxwidth = opt.limitwidth > keymaxwidth? keymaxwidth: opt.limitwidth;
+            $.insertCSS(['.' + classname + ' tbody td'], 'padding-left:' + (keymaxwidth+15) + 'px;');
+            $.insertCSS(['.' + classname + ' tbody td:before'], 'width:' + (keymaxwidth+5) + 'px;');
             return classname;
         }
+        if(!opt.limitheight){
+            $.insertCSS(['.' + classname + ' tbody tr.close'], 'height:' + opt.limitheight + 'px;');
+
+            $list.each(function(index,item){
+                $(item).click(function(){
+                    $(this).toggleClass('close');
+                })
+            })
+        }
         $this.addClass(inital());
-        $this.addClass("GridTable");
-        $this.attr('role','grid table');
+        $this.attr('role', 'grid table');
     };
 
-    $(document).ready(function() {
-        $("[data-gridtable]").each(function(index, item) {
-            $(item).each(function(index, item) {
-                var option = {
-                    columns: $(item).find("thead th").map(function(index,item) {
-                        return {
-                            text: $(item).text(),
-                            colspan: $(item).attr('colspan') * 1 || 1
-                        };
-                    })
-                };
-                $(item).gridtable(option);
+    $(document).on('dom.load', function () {
+        $("[data-gridtable]").each(function (index, item) {
+            $(item).gridtable({
+                limitwidth: $(item).attr('data-limitwidth')*1,
+                key: $(item).attr('data-key'),
+                limitheight:  $(item).attr('data-limitheight')*1,
             });
             $(item).removeAttr('data-gridtable');
         });
