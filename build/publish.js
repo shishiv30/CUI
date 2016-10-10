db76a1dfcb4734e145fd24917c46904d4ed0aef7
Conjee Zou <czou@movoto.com>
2016-10-10 17:19:17 +0800

    add build js

---
+++ build/publish.js
@@ -0,0 +1,9 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+
+ghpages.publish(path.join(__dirname, '../dist'), {
+    branch:'fasdf'
+}, (err) => {
+    if (err) console.log(err);
+    else consle.log('Successfully Published!!!!')
+});
