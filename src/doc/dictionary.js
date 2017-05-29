07215e04751df1a4567d347f20ebd33a4924436c
Conjee <czou@movoto.com>
2017-05-29 08:51:53 +0800

    dictionary

---
+++ src/doc/dictionary.js
@@ -0,0 +1,21 @@
+var lang = {
+    en: {
+        'color': 'color solution',
+        'color_1': 'The regular color we used.',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+        'key': 'value',
+
+
+    },
+    ch{}
+
+};
