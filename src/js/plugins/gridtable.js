51cfd2832fc1a70834a248d80ce79be639f17fda
Conjee <czou@movoto.com>
2017-06-29 18:13:11 +0800

    update responsive table

--- src/js/plugins/gridtable.js
+++ src/js/plugins/gridtable.js
@@ -1,25 +1,23 @@
-(function ($) {
+(function($) {
     var gridtableConfig = {
         name: 'gridtable',
         defaultOpt: {
-            limitwidth: 150,
             key: 'thead th',
-            limitheight: 40
         },
-        initBefore: function () {
+        initBefore: function() {
 
         },
-        init: function (context) {
+        init: function(context) {
             var opt = context.opt;
             var $this = context.$element;
             var $key = $this.find(opt.key);
             var $list = $this.find('tbody tr');
-            var inital = function () {
+            var inital = function() {
                 var classname = 'table-' + +new Date();
                 var colIndex = 0;
                 var fontsize = $key.css('fontSize').replace(/[a-z]/g, '');
                 var keymaxwidth = 0;
-                var columns = $key.map(function (index, item) {
+                var columns = $key.map(function(index, item) {
                     return {
                         text: $(item).text() || '',
                         colspan: $(item).attr('colspan') * 1 || 1
@@ -28,7 +26,9 @@
                 for (var i = 0; i < columns.length; i++) {
                     var column = columns[i];
                     colIndex = colIndex + 1;
-                    $.insertCSS(['.' + classname + ' td:nth-of-type(' + colIndex + '):before'], 'content:"' + column.text + ':";');
+                    $list.each(function() {
+                        $(this).find('td').eq(i).attr('data-th', column.text);
+                    })
                     if (column.colspan > 1) {
                         colIndex = colIndex + column.colspan - 1;
                     }
@@ -37,17 +37,11 @@
                         keymaxwidth = keywidth;
                     }
                 }
-                keymaxwidth = opt.limitwidth > keymaxwidth ? keymaxwidth : opt.limitwidth;
-                $.insertCSS(['.' + classname + ' tbody td'], 'padding-left:' + (keymaxwidth + 15) + 'px;');
-                $.insertCSS(['.' + classname + ' tbody td:before'], 'width:' + (keymaxwidth + 5) + 'px;');
-                if (opt.limitheight > 0) {
-                    $.insertCSS(['.' + classname + ' tbody tr.close'], 'max-height:' + opt.limitheight + 'px;');
-                }
                 $list.addClass('close');
                 return classname;
             };
-            $list.each(function (index, item) {
-                $(item).click(function () {
+            $list.each(function(index, item) {
+                $(item).click(function() {
                     if (!$(this).hasClass('open')) {
                         $list.filter('.open').removeClass('open').addClass('close');
                         $(this).addClass('open').removeClass('close');
@@ -66,8 +60,8 @@
         isThirdPart: false,
     };
     $.CUI.plugin(gridtableConfig);
-    $(document).on('dom.load', function () {
-        $('[data-gridtable]').each(function (index, item) {
+    $(document).on('dom.load', function() {
+        $('[data-gridtable]').each(function(index, item) {
             var data = $(item).data();
             $(item).gridtable(data);
             $(item).removeAttr('data-gridtable');
