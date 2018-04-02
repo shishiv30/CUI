c3c9de0ec97a8a100ae34dce9cd9716fe6c675d0
conjee <conjee@conjees-MacBook-Pro.local>
2018-04-02 00:05:47 -0700

    add demo2

--- app.js
+++ app.js
@@ -12,25 +12,52 @@ var setLang = function (req, res) {
     i18n.setLocale([req, res.locals], lang);
     return lang;
 };
+var url = '//' + hostname + ':' + port + '/';
+var cdnUrl = url + 'dist/src/';
 app.get('/demo1', function (req, res) {
-    var lang = setLang(req, res);
-    var ejsPath =  __dirname + '/src/demo1/' + 'demo1.ejs';
-    ejs.renderFile(ejsPath, {
-        rootUrl: '//' + hostname + ':' + port + '/',
-        lang: lang
-    }, function (err, result) {
-        res.send(result);
-    });
+    try {
+        var lang = setLang(req, res);
+        var ejsPath = __dirname + '/src/demo1/' + 'demo1.ejs';
+        ejs.renderFile(ejsPath, {
+            url: url,
+            cdnUrl: cdnUrl,
+            lang: lang
+        }, function (err, result) {
+            res.send(result);
+        });
+    } catch(e) {
+        console.log(e);
+    }
+});
+app.get('/demo1-2', function (req, res) {
+    try {
+        var lang = setLang(req, res);
+        var ejsPath = __dirname + '/src/demo1/' + 'demo1-2.ejs';
+        ejs.renderFile(ejsPath, {
+            url: url,
+            cdnUrl: cdnUrl,
+            lang: lang
+        }, function (err, result) {
+            res.send(result);
+        });
+    } catch(e) {
+        console.log(e);
+    }
 });
 app.get('/:lang', function (req, res) {
-    var lang = setLang(req, res);
-    var ejsPath =  __dirname + '/src/doc/' + 'index.ejs';
-    ejs.renderFile(ejsPath, {
-        rootUrl: '//' + hostname + ':' + port + '/',
-        lang: lang
-    }, function (err, result) {
-        res.send(result);
-    });
+    try {
+        var lang = setLang(req, res);
+        var ejsPath = __dirname + '/src/doc/' + 'index.ejs';
+        ejs.renderFile(ejsPath, {
+            url: url,
+            cdnUrl: cdnUrl,
+            lang: lang
+        }, function (err, result) {
+            res.send(result);
+        });
+    } catch(e) {
+        console.log(e);
+    }
 });
 i18n.configure({
     locales: ['en', 'ch'],
@@ -38,7 +65,6 @@ i18n.configure({
     directory: __dirname + '/src/doc/locales/'
 });
 app.use(i18n.init);
-
 app.listen(port, hostname, () => {
     /*eslint no-console: ["error", { allow: ["log"] }] */
     console.log(`Server running at http://${hostname}:${port}/`);
--- html.js
+++ html.js
@@ -11,13 +11,15 @@ i18n.configure({
 
 function ejs2html(ejsName,ejsFolder) {
     var htmlPath = '/public/',
-        ejsPath = __dirname + ejsFolder + ejsName;
+        ejsPath = __dirname + ejsFolder + ejsName,
+        url = '%RootUrl%',
+        cdnUrl = url + 'dist/src/';
     i18n.setLocale('ch');
     htmlPath = __dirname + htmlPath;
     if (!fs.existsSync(htmlPath)){
         fs.mkdirSync(htmlPath);
     }
-    ejs.renderFile(ejsPath, {rootUrl:'%RootUrl%'}, function (err, result) {
+    ejs.renderFile(ejsPath, {url:url,cdnUrl:cdnUrl}, function (err, result) {
         if (!err) {
             var htmlName = ejsName.replace('.ejs', '.html');
             fs.writeFile(htmlPath + htmlName, result, function () {});
@@ -30,3 +32,4 @@ function ejs2html(ejsName,ejsFolder) {
 
 ejs2html('index.ejs','/src/doc/');
 ejs2html('demo1.ejs','/src/demo1/');
+ejs2html('demo1-2.ejs','/src/demo1/');
